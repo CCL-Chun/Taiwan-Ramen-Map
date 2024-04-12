7e9c5e02a8bb70e70551d752d1ef56b1ca310f94
CCL-Chun <kevin10218427@gmail.com>
2024-04-13 02:48:57 +0800

    feature: crawl all need info and save to MongoDB Crawl all need info from google maps and parse as JSON-like format to save into cloud MongoDB

--- crawler_selenium.py
+++ crawler_selenium.py
@@ -8,14 +8,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from operator import itemgetter
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
 import logging
-import boto3
+import requests
+from datetime import datetime
+import base64
+import json
 import time
 import re
+import os
 
 logging.basicConfig(level=logging.INFO,filename='log.txt',filemode='a',
     format='%(asctime)s %(filename)s %(levelname)s:%(message)s')
+
+## connect to cloud MongoDB
+try:
+    load_dotenv()
+    username = os.getenv("MongoDB_user")
+    password = os.getenv("MongoDB_password")
+    cluster_url = os.getenv("MongoDB_cluster_url")
+    uri = f"mongodb+srv://{username}:{password}@{cluster_url}?retryWrites=true&w=majority&appName=ramen-taiwan"
+    client = MongoClient(uri, server_api=ServerApi('1')) # Create a new client and connect to the server
+    db = client['ramen-taiwan']
+    collection = db['ramen_info']
+except Exception as e:
+    logging.error(f"Cannot connect to MongoDB!{e}")
 
 def wait_for_element_location_to_be_stable(element):
     initial_location = element.location
@@ -40,66 +59,129 @@ def click_more_buttons():
             print(more_buttons)#this will tell us how many more buttons are currently loaded
             print(body.text)
 
-## check S3 connection
-# s3_client = boto3.client('s3')
 
-## read the ramen list
-# restaurant_name = []
-# with open("test_ramen_list.txt",'r') as input:
-#     for line in input:
-#         restaurant_name.append(re.sub(r'\((\d+|\w)*(開幕|預定|試賣|未定|試營運|預計)*\)|\((\w|\d+)*停\w*業*\)',
-#                                 '', line.strip()))
+# read the ramen list
+restaurant_name = []
+with open("ramen_list.txt",'r') as input:
+    for line in input:
+        restaurant_name.append(re.sub(r'\((\d+|\w)*(開幕|預定|試賣|未定|試營運|預計)*\)|\((\w|\d+)*停\w*業*\)',
+                                '', line.strip()))
 
 ## start browser
-website = 'https://www.google.com.tw/maps'
+google_maps = 'https://www.google.com.tw/maps'
 options = Options()
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')  
 driver = webdriver.Edge(options=options)
-# driver.get(website)
-# actionChains = ActionChains(driver)
 
-# for ramen in restaurant_name:
-for ramen in ['拉麵公子']:
+for ramen in restaurant_name:
     ## start browser
-    driver.get(website)
+    driver.get(google_maps)
     actionChains = ActionChains(driver)
-    ###### go to the ramen restaurant
+    ## go to the ramen restaurant
     actionChains.send_keys(ramen)  # Send keys to the element
     actionChains.send_keys(Keys.ENTER)  # Press Enter key
     actionChains.perform()  # Perform the actions
     wait = WebDriverWait(driver, 5)
+
+    ## get the name
+    try:
+        heading_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h1.DUwDvf')))
+        title = heading_element.text
+        print(title)
+    except Exception as e:
+        logging.error(f"cannot get name: {ramen}!{e}")
+        title = ramen
+
     ## go to opening time
     try:
-        opening_buttons = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "RcCsl")))
-        opening_buttons[1].click()
-        time_all = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "y0skZc")))
+        opening_buttons = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'button[data-item-id="oh"]')))
+        actionChains.scroll_to_element(opening_buttons[0]).perform()
+        opening_buttons[0].click()
+    except TimeoutException:
+        try:
+            opening_buttons = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME,'o0Svhf')))
+            actionChains.scroll_to_element(opening_buttons[0]).perform()
+            opening_buttons[0].click()
+        except Exception as e:
+            logging.error(f"Find no element of opening time: {ramen}!{e}")
+    except Exception as e:
+        logging.exception(f"cannot get opening time of {ramen}!")
 
+    try:
+        time_all = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "y0skZc")))
         open_time = {}
+
         for time_detail in time_all:
             day = time_detail.find_element(By.CLASS_NAME,'ylH6lf').text
             duration = [item.text for item in time_detail.find_elements(By.CLASS_NAME,'G8aQO')]
-            open_time[day] = duration
+            if day:
+                open_time[day] = duration
     except Exception as e:
-        logging.exception(f"cannot get opening time of {ramen}!Exception: {e}")
-    ## get back
-    back_button = driver.find_element(By.CSS_SELECTOR,'button[aria-label="返回"]')
-    back_button.click()
-    time.sleep(3)
+        logging.error(f"Error while crawling opening time: {ramen}!{e}")
+    
+    ## get back page if need
+    try:
+        back_button = driver.find_element(By.CSS_SELECTOR,'button[aria-label="返回"]')
+        back_button.click()
+    except Exception:
+        logging.info(f"no need to get back from opening time page: {ramen}!")
+
+    ## get img url
+    img_button = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "ZKCDEc")))
+    time.sleep(1)
+    image_element = img_button[0].find_element(By.TAG_NAME, 'img')
+    img_url = image_element.get_attribute('src')
+
+    ## img to base64
+    try:
+        response = requests.get(img_url,timeout=10)
+        image_content = response.content
+        base64_string = base64.b64encode(image_content)
+        base64_string = base64_string.decode('utf-8')
+        base64_image_url = f'data:image/png;base64,{base64_string}'
+    except Exception as e:
+        logging.exception(f"cannot convert url to base64 for {ramen}!Exception: {e}")
+
+    ## get official website
+    try:
+        website = driver.find_element(By.CSS_SELECTOR,'a[data-item-id="authority"]').get_attribute('href')
+    except Exception as e:
+        logging.exception(f"cannot find website of {ramen} !Exception: {e}")
+
+    ## get address
+    try:
+        address = driver.find_element(By.CSS_SELECTOR,'button[data-item-id="address"]').get_attribute('aria-label')
+        address = re.sub(r'(地址: \d+)','',address)
+    except Exception as e:
+        logging.exception(f"cannot find address of {ramen} !Exception: {e}")
 
     ## go to reviews
     try:
         main_buttons = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "Gpq6kf")))
     except Exception as e:
-        logging.error(f"{ramen} not done yet!Error: {e}")
+        logging.error(f"{ramen} reviews not done yet!Error: {e}")
         continue
 
     actionChains.move_to_element(main_buttons[1]).perform()
-    # print(main_buttons[1].text)
     main_buttons[1].click()
 
-    break_condition = True
-
+    ## get stats rating
+    ## mean rating
+    mean_rating_element = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]')))
+    mean_rating = mean_rating_element[0].text
+    overall_rating = {"mean":mean_rating}
+    ## rating distribution
+    rating_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'tr.BHOKXe')))
+    for element in rating_elements:
+        aria_label = element.get_attribute('aria-label')
+        parts = aria_label.split()
+        star_rating = int(parts[0]) 
+        review_count = ''.join(filter(str.isdigit, parts[1])) # remove commas and other words
+        review_count = int(review_count)  
+        overall_rating[f'amount_{star_rating}'] = review_count
+    
+    break_condition = False
     while not break_condition:
         try:
             end_of_reviews = driver.find_elements(By.CLASS_NAME, 'qjESne')
@@ -119,18 +201,44 @@ for ramen in ['拉麵公子']:
         temp = results[-1]
         wait_for_element_location_to_be_stable(temp)
 
-        if len(results) > 10:
+        if len(results) > 100:
             break_condition = True
 
+    ## get coordination
     get_url = driver.current_url
-    print("The current url is:"+str(get_url))
-    print(open_time)
-    # for i, result in enumerate(results, start=0):
-    #     comment_span = result.find_element(By.CLASS_NAME, 'wiI7pd')
-    #     user_url = result.find_element(By.CLASS_NAME, "al6Kxe").get_attribute("data-href")
-    #     user_rating = result.find_element(By.CLASS_NAME, "kvMYJc").get_attribute("aria-label")
-    #     print(f"Result {i}: ['{user_url}','{user_rating}','{comment_span.text}']") # user name & rating not yet
+    coord = re.search(r'@([-\d.]+),([-\d.]+)',get_url)
+    latitude = coord.group(1)
+    longitude = coord.group(2)
 
+    reviews = []
+    for i, result in enumerate(results, start=0):
+        comment_span = result.find_element(By.CLASS_NAME, 'wiI7pd')
+        user_url = result.find_element(By.CLASS_NAME, "al6Kxe").get_attribute("data-href")
+        user_rating = result.find_element(By.CLASS_NAME, "kvMYJc").get_attribute("aria-label").split()[0]
+        user_name = result.find_element(By.CLASS_NAME, "d4r55").text
+        user_id = re.search(r'(?<=contrib/)\d+', user_url).group(0)
+        reviews.append({"user_id":user_id,"user_url":user_url,"user_name":user_name,
+                        "rating":user_rating,"comment":comment_span.text})
+
+    crawling_result = {
+        "name":title,
+        "maps_url":get_url,
+        "img_url":img_url,
+        "img_base64":base64_image_url,
+        "open_time":open_time,
+        "website":website,
+        "overall_rating":overall_rating,
+        "address":address,
+        "latitude":latitude,
+        "longitude":longitude,
+        # "PlaceID":place_id,
+        "reviews":reviews,
+        "create_time":datetime.utcnow(),
+        "update_time":datetime.utcnow()
+    }
+
+    insertion = collection.insert_one(crawling_result)
+    logging.info(f"Data inserted with _id: {insertion.inserted_id}")
+    time.sleep(1)
 
 driver.quit()
-
