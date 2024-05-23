9ab71b0a76cba0401ccb994705a8c0ba07c8dfd3
CCL-Chun <kevin10218427@gmail.com>
2024-05-23 14:00:56 +0000

    refactor: Restructure Flask application following MVC pattern
    1. Model: Introduce Database.py to manage MongoDB connection and Redis.py for Redis connection.
    2. View: Serve the leaflet_map.html for the map view.
    3. Controller: Separate API routes based on feature functionality into four files - traffic_controller, ramen_controller, search_controller, and socketio_controller.

---
+++ application/server/models/Database.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from flask import current_app
+import logging
+
+class MongoDBConnection:
+    def __init__(self, uri, db_name):
+        """Initialize the MongoDB connection."""
+        try:
+            self.client = MongoClient(uri, server_api=ServerApi('1'))
+            self.db = self.client[db_name]
+        except Exception as e:
+            logging.error(f"Cannot connect to MongoDB! {e}")
+            raise e
+
+    def get_collection(self, collection_name):
+        """Retrieve a collection by name from the database."""
+        return self.db[collection_name]
+
+
+
+# Usage example:
+# in __init__.py
+#=================
+# global mongo_connection
+# mongo_connection = MongoDBConnection(app.config['MONGO_URI'], app.config['DB_NAME'])
+#=================
+# in controller.py
+#=================
+# from ..models.Database import mongo_connection
+# collection_ramen = mongo_connection.get_collection('ramen_info')
+#=================
