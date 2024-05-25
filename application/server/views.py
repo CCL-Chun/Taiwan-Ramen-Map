1a37f9d1540a9ba75264348ef927c4b5218bb898
CCL-Chun <kevin10218427@gmail.com>
2024-05-25 09:48:05 +0000

    hotfix: Add health route for app health check

--- application/server/views.py
+++ application/server/views.py
@@ -4,3 +4,7 @@ from server import app
 @app.route("/")
 def first_view():
     return render_template('leaflet_map.html')
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "ok"}), 200
